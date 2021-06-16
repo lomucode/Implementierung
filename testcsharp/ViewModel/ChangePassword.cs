796f2c48e0d6b35bc0af21c42bd413f272f505f6
olispcvs <oliver.specovius@student.uni-siegen.de>
2021-06-16 20:26:32 +0200

    Alles up2date

---
+++ testcsharp/ViewModel/ChangePassword.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testcsharp.ViewModel
+{
+    public class ChangePassword
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Aktuelles Passwort")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Neues Passwort")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Passwort bestätigen")]
+        [Compare("NewPassword", ErrorMessage =
+            "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
