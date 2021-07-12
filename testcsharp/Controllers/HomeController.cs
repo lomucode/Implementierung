c39755e050a67da87c2b0bc5a6d54eaeffa78aa4
olispcvs <83537940+olispcvs@users.noreply.github.com>
2021-07-12 17:33:01 +0200

    homepage new

--- testcsharp/Controllers/HomeController.cs
+++ testcsharp/Controllers/HomeController.cs
@@ -96,6 +96,7 @@ namespace testcsharp.Controllers
         {
             return View();
         }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
